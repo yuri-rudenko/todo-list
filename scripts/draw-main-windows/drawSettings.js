01fffe103aa3ea02ad9690e36b505131ac03707d
Yurii <yurii.rudenko26@gmail.com>
2023-09-25 00:36:26 +0300

    added-tag-adding-and-removing

--- scripts/draw-main-windows/drawSettings.js
+++ scripts/draw-main-windows/drawSettings.js
@@ -17,7 +17,7 @@ export function drawSettings(Work) {
         tagDel.classList.add('delete-tag')
 
         tagDel.insertAdjacentHTML('beforeend', `<p>${tag}</p>`)
-        tagDel.insertAdjacentHTML('beforeend', `<div class="delete-tag-button"><p>×</p></div>`)
+        tagDel.insertAdjacentHTML('beforeend', `<div class="delete-tag-button"><p class="del-tag-cross">×</p></div>`)
 
         tagWrapper.append(tagDel)
 
@@ -40,7 +40,34 @@ export function drawSettings(Work) {
     }
     
     document.querySelector('#settings-template').before(settings)
+
+    /// add logic
+
+    let addTag = settings.querySelector('.submit-new-tag')
+    addTag.addEventListener('click', () => {
+        let tagName = settings.querySelector('.add-tag input')
+        console.log(tagName)
+        if(tagName.value.length > 0) {
+            let newTag = document.createElement('div')
+            newTag.classList.add('tag')
+            let delTag = document.createElement('div')
+            delTag.classList.add('delete-tag')
+            delTag.insertAdjacentHTML('beforeend', `<p>${tagName.value}</p><div class="delete-tag-button"><p class="del-tag-cross">×</p></div>`)
+            newTag.append(delTag)
+            tags.append(newTag)
+        }
+
+        tagName.value = ''
+    })
+
+    tags.addEventListener('click', (ev) => {
+        if(ev.target.classList.contains('del-tag-cross')) {
+            ev.target.parentElement.parentElement.parentElement.remove()
+        }
+    })
+    
 }
+// <div class="delete-tag"></div>
 
 export function updateSettings(Work) {
     let name = document.querySelector('.name-change input')
@@ -59,7 +86,7 @@ export function updateSettings(Work) {
         tagDel.classList.add('delete-tag')
 
         tagDel.insertAdjacentHTML('beforeend', `<p>${tag}</p>`)
-        tagDel.insertAdjacentHTML('beforeend', `<div class="delete-tag-button"><p>×</p></div>`)
+        tagDel.insertAdjacentHTML('beforeend', `<div class="delete-tag-button"><p class="del-tag-cross">×</p></div>`)
 
         tagWrapper.append(tagDel)
 
